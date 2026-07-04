5aa8bd729fb9738d61dfa61950284261d8349fb1
Brian <wahyubrianala@gmail.com>
2026-07-04 16:12:40 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+# Konfigurasi Path Dataset
+DATA_DIR = "dataset"
+IMAGES_DIR = os.path.join(DATA_DIR, "images", "small")
+METADATA_PATH = os.path.join(DATA_DIR, "images", "metadata", "images.csv.gz")
+
+# Konfigurasi Model
+# Menggunakan model pre-trained (tanpa training ulang)
+MODEL_NAME = "openai/clip-vit-base-patch32" 
+
+# Batasan Skala Dataset
+MAX_SAMPLES = 780
